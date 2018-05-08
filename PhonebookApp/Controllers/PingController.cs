760795d40f1a036bf0710902cd48bf9f6b3d66ad
Зудов Антон <antony.zudov@mail.ru>
2018-05-09 02:22:58 +0400

    Ping-контроллер

---
+++ PhonebookApp/Controllers/PingController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace PhonebookApp.Controllers
+{
+    public class PingController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}

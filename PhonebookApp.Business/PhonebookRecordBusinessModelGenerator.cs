8b7d5b6ba760e1f62349ae729b616918976be43b
Зудов Антон <antony.zudov@mail.ru>
2018-05-09 22:11:27 +0400

    Тесты и логика на парсинг имен в PhonebookRecordBusinessModelGenerator

---
+++ PhonebookApp.Business/PhonebookRecordBusinessModelGenerator.cs
@@ -0,0 +1,50 @@
+﻿using PhonebookApp.Business.Abstract;
+using System;
+using PhonebookApp.Business.Abstract.Models;
+using PhonebookApp.Models;
+using System.Linq;
+
+namespace PhonebookApp.Business
+{
+    public class PhonebookRecordBusinessModelGenerator : IPhonebookRecordBusinessModelGenerator
+    {
+        public PhonebookRecord Generate(PhonebookRecordSaveRequest saveRequest)
+        {
+            char[] charSeparators = new char[] { ',', ' ', '.' };
+            var words = saveRequest.Fullname.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            if (words.Count <= 1)
+            {
+                return new PhonebookRecord
+                {
+                    Id = saveRequest.Id.GetValueOrDefault(0),
+                    Surame = words.FirstOrDefault(),
+                };
+            }
+
+            if (words.Count == 2)
+            {
+                return new PhonebookRecord
+                {
+                    Id = saveRequest.Id.GetValueOrDefault(0),
+                    Name = words.First(),
+                    Surame = words.Last()
+                };
+            }
+
+            var surname = words.LastOrDefault();
+            words.Remove(surname);            
+
+            var patronymic = words.LastOrDefault();
+            words.Remove(patronymic);
+
+            return new PhonebookRecord
+            {
+                Id = saveRequest.Id.GetValueOrDefault(0),
+                Name = string.Join(" ", words),
+                Patronymic = patronymic,
+                Surame = surname
+            };
+        }
+    }
+}
--- PhonebookApp/App_Start/WebApiConfig.cs
+++ PhonebookApp/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace PhonebookApp
             var container = new UnityContainer();
             container.RegisterType<IPingService, PingService>(new HierarchicalLifetimeManager());
             container.RegisterType<IPhonebookRecordService, PhonebookRecordService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IPhonebookRecordBusinessModelGenerator, PhonebookRecordBusinessModelGenerator>(new HierarchicalLifetimeManager());
             container.RegisterType<IPingDao, PingDao>(new HierarchicalLifetimeManager());
             config.DependencyResolver = new UnityResolver(container);
 
